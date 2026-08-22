31927a70bd09bd6decf74a0c4f453e18e62d44a5
harmeetbuilds <harmeetsingh230806@gmail.com>
2026-08-22 21:19:11 +0530

    adding new file

---
+++ Backend/test_email_analyzer.py
@@ -0,0 +1,6 @@
+from src.email_analyzer import analyze_email
+
+email = "Hi, can we schedule a meeting tomorrow at 11 AM to discuss the project progress?"
+result = analyze_email(email)
+
+print(result)
